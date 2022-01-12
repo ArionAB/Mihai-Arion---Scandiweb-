0f77db4c1e05138bf59d4738767bd1ffc89cdc84
Mihai <mihai.arion0@gmail.com>
2022-01-12 21:59:49 +0200

    Product gallery working properly, click on thumblnail image shows the coresponding image

--- src/Pages/productPage/productPage.jsx
+++ src/Pages/productPage/productPage.jsx
@@ -15,6 +15,15 @@ const GET_PRODUCT = gql`
       brand
       name
       gallery
+      inStock
+      description
+      prices {
+        currency {
+          label
+          symbol
+        }
+        amount
+      }
       attributes {
         id
         name
@@ -36,11 +45,12 @@ class ProductPage extends Component {
     this.toggleClass = this.toggleClass.bind(this);
 
     this.state = {
-      item: {},
+      item: [],
       active: false,
       chosenImage: [],
       savedAttributes: [],
       prices: [],
+      index: 0,
     };
   }
   componentDidMount() {
@@ -65,50 +75,95 @@ class ProductPage extends Component {
     this.setState({ item: response.data.product });
   }
 
-  getGallery() {
+  getAttributes() {
     const { item } = this.state;
-    const gallery = item.gallery;
 
-    const entries = Object.entries(gallery ? gallery : "");
-
-    return entries.map((entry) => {
-      console.log(entry);
-      console.log(entry[1]);
-      /*       entry.filter((filtru) => {
-        if (filtru[0] == entry[0]);
-
-        alert("nu e egal");
-      }); */
+    return item.attributes?.map((att) => {
       return (
-        <>
-          <p>{entry[0]}</p>
-          <img
-            className={this.state.active ? "images" : "oneImage"}
-            onClick={this.toggleClass}
-            src={entry[1]}
-            alt=""
-          ></img>
-        </>
+        <div>
+          <div>
+            <p>{att.name}</p>
+          </div>
+          <div>
+            {att.items.map((size, index) => {
+              return <button key={index}>{size.value}</button>;
+            })}
+          </div>
+        </div>
       );
     });
   }
 
-  render() {
+  handleTab(index) {
+    this.setState({ index: index });
+  }
+
+  getGallery() {
     const { item } = this.state;
 
+    const gallery = item.gallery;
+
+    const entries = Object.entries(gallery ? gallery : "");
+
+    return entries.map((entry, index) => {
+      return (
+        <div className="thumb">
+          <img
+            onClick={() => this.handleTab(index)}
+            key={index}
+            src={entry[1]}
+            alt=""
+          ></img>
+        </div>
+      );
+    });
+  }
+  /*   getGallery() {
+    const { item, index } = this.state;
+    const gallery = item.gallery;
+
+    return gallery?.map((entry) => {
+      console.log(entry, index);
+      return (
+        <div className="thumb">
+          <img
+            onClick={() => this.handleTab(index)}
+            key={index}
+            className={this.state.active ? "images" : "oneImage"}
+            src={entry}
+            alt=""
+          ></img>
+        </div>
+      );
+    });
+  } */
+
+  render() {
+    const { item, index } = this.state;
+    const gallery = item.gallery;
+
+    const newObj = Object.assign({}, gallery);
+
     return (
-      <div>
-        <h1>{item.brand}</h1>
+      <div className="container">
+        <img src={newObj[index]} alt="" />
         <div className="gallery">{this.getGallery()}</div>
-        <div></div>
+
+        <div className="specs">
+          <h1>{item.brand}</h1>
+          <h2>{item.name}</h2>
+          <div>{this.getAttributes()}</div>
+          <button>ADD TO CART</button>
+          <p>{item.description}</p>
+        </div>
       </div>
     );
   }
 }
-/* const gallery = item.gallery;
-    for (const photo of gallery) {
-      console.log(photo);
-    } */
+//  const gallery = item.gallery;
+//     for (const photo of gallery) {
+//       console.log(photo);
+//     }
 
 const mapStateToProps = (state) => ({
   prodId: state.product.prodID,
@@ -116,10 +171,10 @@ const mapStateToProps = (state) => ({
 
 export default ProductPage;
 
-/* export default graphql(GET_PRODUCT, {
-  options: { variables: { id } },
-})(ProductPage);
-connect(mapStateToProps)(ProductPage); */
+// /* export default graphql(GET_PRODUCT, {
+//   options: { variables: { id } },
+// })(ProductPage);
+// connect(mapStateToProps)(ProductPage); */
 
 /* export default graphql(GET_PRODUCT, {
   options: { variables: { id } },
