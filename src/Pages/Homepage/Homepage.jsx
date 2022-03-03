0ba9b2c9016fe68b8ad95749ce041bfa7221462a
Mihai <mihai.arion0@gmail.com>
2022-03-03 10:08:32 +0200

    Added animation on scroll without a library

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -5,22 +5,79 @@ import { ReactComponent as Trustpilot } from "../../Assets/trustpilot-svgrepo-co
 import "../Homepage/Homepage.styles.scss";
 
 class Homepage extends Component {
-  constructor() {
-    super();
-    this.myRef = React.createRef();
-    this.seconRef = React.createRef();
-    this.thirdRef = React.createRef();
-    this.state = {
-      itemOne: false,
-      itemTwo: false,
-      itemThree: false,
-    };
-    /*     const myRef = 
-        seconRef = useRef(null),
-        thirdRef = useRef(null); */
+  componentDidMount() {
+    this.observer();
   }
 
-  componentDidMount() {
+  observer() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // Add the CSS class that has your animation
+          entry.target.classList.add("fade-in");
+          return;
+        }
+        // line below will stop from triggering animation everytime is being scrolled
+        observer.unobserve(entry.target);
+        // entry.target.classList.remove("fade-in");
+        //line above triggers animation everytime is being scrolled
+      });
+    });
+
+    // Observe the element
+    const elementLeft = document.querySelectorAll(".reveal-left");
+    const elementRight = document.querySelectorAll(".reveal-right");
+
+    elementLeft.forEach((elem) => {
+      observer.observe(elem);
+    });
+    elementRight.forEach((elem) => {
+      observer.observe(elem);
+    });
+  }
+
+  render() {
+    return (
+      <div className="HP-container">
+        <div className="order">
+          <div className="left reveal-left ">
+            <h1 className="">
+              High quality clothes & tech, <span>delivered</span> to your door.
+            </h1>
+            <p className="reveal">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500.
+            </p>
+            <Link to="/category/all">
+              <button className="reveal">Place an Order</button>
+            </Link>
+            <div className="trustpilot reveal">
+              <p className="asd">
+                <Trustpilot />
+                Trustpilot
+              </p>
+
+              <p className="reveal">
+                <span className="numbers">4.8 out of 5</span> based on 2000+
+                reviews
+              </p>
+            </div>
+          </div>
+          <div className="right reveal-right">
+            <div className="background"></div>
+            <img src={require("../../Assets/RockPS.png")} alt="Rock" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
+
+/*  //Works but other method is easier
+  onScrollAnimation() {
     const topPosition = (element) => element.getBoundingClientRect().top;
     const div1Pos = topPosition(this.myRef.current);
     const div2Pos = topPosition(this.seconRef.current);
@@ -30,7 +87,7 @@ class Homepage extends Component {
       const scrollPosition = window.scrollY + window.innerHeight;
       // console.log(scrollPosition, "scroll");
       // console.log(scrollPosition - div2Pos)
-      console.log(scrollPosition - div3Pos < 50, scrollPosition - div2Pos);
+      // console.log(scrollPosition - div3Pos < 50, scrollPosition - div2Pos);
 
       if (div1Pos < scrollPosition) {
         this.setState((state) => ({ ...state, itemOne: true }));
@@ -44,86 +101,39 @@ class Homepage extends Component {
     };
     window.addEventListener("scroll", onScroll);
     return () => window.removeEventListener("scroll", onScroll);
-  }
-  /* componentDidUpdate() {
-    const topPosition = (element) => element.getBoundingClientRect().top;
-    const div1Pos = topPosition(this.myRef.current);
-    const div2Pos = topPosition(this.seconRef.current);
-    const div3Pos = topPosition(this.thirdRef.current);
-    const onScroll = () => {
-      const scrollPosition = window.scrollY + window.innerHeight;
-      if (div1Pos < scrollPosition) {
-        this.setState((state) => ({ ...state, itemOne: true }));
-      }
-      if (div2Pos < scrollPosition) {
-        this.setState((state) => ({ ...state, itemTwo: true }));
-      }
-      if (div3Pos < scrollPosition) {
-        this.setState((state) => ({ ...state, itemThree: true }));
-      }
-    };
-    window.addEventListener("scroll", onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
   } */
-  render() {
-    const { itemOne, itemTwo, itemThree } = this.state;
-    // console.log(itemOne, itemTwo, itemThree);
-    return (
-      <div className="HP-container">
-        <div className="order">
-          <div className="left">
-            <h1>
-              High quality clothes & tech, <span>delivered</span> to your door.
-            </h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500.
-            </p>
-            <Link to="/category/all">
-              <button>Place an Order</button>
-            </Link>
-            <div className="trustpilot">
-              <p className="asd">
-                <Trustpilot />
-                Trustpilot
-              </p>
 
-              <p>
-                <span className="numbers">4.8 out of 5</span> based on 2000+
-                reviews
-              </p>
-            </div>
-          </div>
-          <div className="right">
-            <div className="background"></div>
-            <img src={require("../../Assets/RockPS.png")} alt="Rock" />
-          </div>
-        </div>
-        <div className={itemOne ? "test-active" : "test"} ref={this.myRef}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga ratione
-          reprehenderit, in esse numquam cumque minima at magnam, perferendis
-          maxime amet perspiciatis aut ut obcaecati. Porro quibusdam quisquam
-          nobis cum?
-        </div>
-        <div className={itemTwo ? "test1-active" : "test1"} ref={this.seconRef}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse at
-          repellendus fugiat molestiae vero. Modi ipsum dolorum atque sequi,
-          magni quibusdam qui reprehenderit ea sed, voluptatibus sit ratione nam
-          voluptate?
-        </div>
-        <div
-          className={itemThree ? "test2-active" : "test2"}
-          ref={this.thirdRef}
-        >
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
-          voluptate deleniti nihil provident eveniet ipsum odit impedit neque
-          modi. Recusandae dolor at mollitia enim quibusdam laboriosam a
-          reiciendis nemo excepturi?
-        </div>
-      </div>
-    );
+/* arturo() {
+    const hidden_elements = document.querySelectorAll(".reveal");
+
+    function reveal() {
+      for (let hidden of hidden_elements) {
+        const elementTop = hidden.getBoundingClientRect().top;
+        const distanceSlow = 150;
+        const distanceQuick = 25;
+        switch (hidden.classList.contains("reveal-quick")) {
+          case true:
+            if (elementTop < window.innerHeight - distanceQuick) {
+              hidden.classList.add("active");
+              setTimeout(() => hidden.classList.add("quick-animate"), 800);
+            }
+            break;
+          default:
+            if (elementTop < window.innerHeight - distanceSlow) {
+              hidden.classList.add("active");
+              setTimeout(() => hidden.classList.add("quick-animate"), 800);
+            }
+        }
+      }
+    }
+    /* .reveal {
+  transform: translateY(10rem);
+  opacity: 0;
+  transition: transform 0.8s ease-out, opacity 0.8s ease-out;
+  &.active {
+    transform: none;
+    opacity: 1;
   }
-}
-
-export default Homepage;
+} */
+// window.addEventListener("scroll", reveal);
+// }
